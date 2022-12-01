69c7fa39af8db88c5404b1ae64cebf478e0b9bb6
Michal <michal.jaworski@zavamed.com>
2022-12-01 11:31:04 +0000

    update dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,78 +1,77 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'borzoj/kirby',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '29e082427d39ff7b51384ce7b6e2b963ce80e7ab',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '34c3ab4331479616b0c9d67fe0b06d780160deca',
-    'name' => 'getkirby/kirby',
-  ),
-  'versions' => 
-  array (
-    'erusev/parsedown' => 
-    array (
-      'pretty_version' => '1.7.1',
-      'version' => '1.7.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '92e9c27ba0e74b8b028b111d1b6f956a15c01fc1',
+    'versions' => array(
+        'borzoj/kirby' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '29e082427d39ff7b51384ce7b6e2b963ce80e7ab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'borzoj/toolkit' => array(
+            'pretty_version' => '2.5.15',
+            'version' => '2.5.15.0',
+            'reference' => 'a760e15810354c47bc1e4ddf7d0e06eaf700433f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../borzoj/toolkit',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.7.1',
+            'version' => '1.7.1.0',
+            'reference' => '92e9c27ba0e74b8b028b111d1b6f956a15c01fc1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'erusev/parsedown-extra' => array(
+            'pretty_version' => '0.7.1',
+            'version' => '0.7.1.0',
+            'reference' => '0db5cce7354e4b76f155d092ab5eb3981c21258c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown-extra',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'filp/whoops' => array(
+            'pretty_version' => '2.1.14',
+            'version' => '2.1.14.0',
+            'reference' => 'c6081b8838686aa04f1e83ba7e91f78b7b2a23e6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../filp/whoops',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'michelf/php-smartypants' => array(
+            'pretty_version' => '1.8.1',
+            'version' => '1.8.1.0',
+            'reference' => '47d17c90a4dfd0ccf1f87e25c65e6c8012415aad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../michelf/php-smartypants',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'erusev/parsedown-extra' => 
-    array (
-      'pretty_version' => '0.7.1',
-      'version' => '0.7.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0db5cce7354e4b76f155d092ab5eb3981c21258c',
-    ),
-    'filp/whoops' => 
-    array (
-      'pretty_version' => '2.1.14',
-      'version' => '2.1.14.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c6081b8838686aa04f1e83ba7e91f78b7b2a23e6',
-    ),
-    'getkirby/kirby' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '34c3ab4331479616b0c9d67fe0b06d780160deca',
-    ),
-    'getkirby/toolkit' => 
-    array (
-      'pretty_version' => '2.5.14',
-      'version' => '2.5.14.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'cf2720b737b6ae5bcdbd1c84e820c3e86001b3a3',
-    ),
-    'michelf/php-smartypants' => 
-    array (
-      'pretty_version' => '1.8.1',
-      'version' => '1.8.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '47d17c90a4dfd0ccf1f87e25c65e6c8012415aad',
-    ),
-    'psr/log' => 
-    array (
-      'pretty_version' => '1.1.3',
-      'version' => '1.1.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
-    ),
-  ),
 );
